d5ea95ab5e28d973d422694cccac33d5d6ec9555
Elnatan Emanuel Lima Santos <elnatan.santos@ctis.com.br>
2020-07-03 22:15:51 -0300

    Criação do projeto

---
+++ src/test/java/com/desafio/mv/DesafioMV/DesafioMvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.mv.DesafioMV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
