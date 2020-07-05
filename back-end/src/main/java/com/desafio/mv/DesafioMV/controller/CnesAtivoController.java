99184bd786bd3d5544de225434ba133cd74b11b9
Elnatan Emanuel Lima Santos <elnatan.santos@ctis.com.br>
2020-07-04 22:21:35 -0300

    criação do swagger

--- back-end/src/main/java/com/desafio/mv/DesafioMV/controller/CnesAtivoController.java
+++ back-end/src/main/java/com/desafio/mv/DesafioMV/controller/CnesAtivoController.java
@@ -13,30 +13,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.desafio.mv.DesafioMV.service.CnesService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/cnes-ativos")
+@Api(value = "API REST de leitura de CNES Ativos")
 public class CnesAtivoController {
 	
 	@Autowired
 	private  CnesService cnesService;
 	
 	@GetMapping
+	@ApiOperation(value = "retorna um lista com todos CNES paginado ")
 	public ResponseEntity<?> getAllCnesAtivos(Pageable pageable) {
 		return new ResponseEntity<>(this.cnesService.getAll(pageable), HttpStatus.OK);
 		
 	}
 	
 	@GetMapping("estados")
+	@ApiOperation(value = "retorna um lista com a sigla de todos estados cadastrados")
 	public ResponseEntity<?> getAllEstados(){
 		return new ResponseEntity<>(this.cnesService.getEstado(), HttpStatus.OK);
 	}
 	
 	@GetMapping("estados/{sigla}")
+	@ApiOperation(value = "retorna um lista com todos CNES de um dertimanado estado  ")
 	public ResponseEntity<?> getAllCnesPorEstado(@PathVariable(value = "sigla") String siglaUF, Pageable pageable){
 		return new ResponseEntity<>(this.cnesService.getEstadoPrSigla(siglaUF, pageable), HttpStatus.OK);
 	}
 	
 	@GetMapping("tipos-de-unidade")
+	@ApiOperation(value = "retorna um lista com todas CNES por tipo de unidade ")
 	public ResponseEntity<?> getAllCnesPorTipoUnidade(@RequestParam("tipoDeUnidade") String tipoDeUnidade, Pageable pageable){
 		
 		return new ResponseEntity<>(this.cnesService.getAllCnesPorTipoUnidade(tipoDeUnidade, pageable), HttpStatus.OK);
